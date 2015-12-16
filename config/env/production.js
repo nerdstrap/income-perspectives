8a42a36f6211ffb860dc10f57607c053a616cf0e
Michael Baltic <geekstrap@live.com>
2015-12-16 11:09:25 -0500

    added heroku mongodb

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	//db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
-	db: 'mongodb://nerdstrap:Jock(5^0@ds056288.mongolab.com:56288/mean-prod',
+	db: 'mongodb://heroku_6w5tbb8v:j1rjbvfnfpqav90kc3qaj3aicf@ds033125.mongolab.com:33125/heroku_6w5tbb8v',
 	/**
 	 * Database options that will be passed directly to mongoose.connect
 	 * Below are some examples.
